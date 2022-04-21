5403f7ff2fa0a9059ca93140a46248e09de25a5f
yoza kazuki <yzkzk0323@gmail.com>
2022-04-21 19:59:25 +0900

    各処理のコメントを追加

--- assets/js/varidation.js
+++ assets/js/varidation.js
@@ -5,7 +5,7 @@ Vue.createApp({
             mail: '', //メールアドレス
             subject: '', //件名
             contents: '', //内容
-            errors: {
+            errors: { //各項目のエラーメッセージ
                 name: [],
                 mail: [],
                 subject: [],
@@ -22,7 +22,7 @@ Vue.createApp({
             let contents_error_message = [];
             // タイプを日本語に変換
             let type_ja = {
-                name: 'お名前',
+                name: '名前',
                 mail: 'メールアドレス',
                 subject: '件名',
                 contents: '内容'
@@ -31,45 +31,45 @@ Vue.createApp({
             console.log(max);
             // メッセージを配列に格納
             let message = [
-                max + '文字以内で入力してください。',
-                type_ja[type] + 'が未入力です。',
+                max + '文字以内で入力してください。',//message[0]
+                type_ja[type] + 'が未入力です。',//message[1]
 
             ];
             // 入力された値を取得
             // 名前
             if (type === 'name') {
                 if (this.name.length > max) {// 入力された文字数がmaxを超えている場合
-                    name_error_message.push(message[0]);
+                    name_error_message.push(message[0]); //エラーメッセージを配列に格納
                 }else if(this.name === ''){// 入力されていない場合
-                    name_error_message.push(message[1]);
+                    name_error_message.push(message[1]); //エラーメッセージを配列に格納
                 }
-                this.errors.name = name_error_message
+                this.errors.name = name_error_message //エラーメッセージをerrors.nameに格納
             }
             if (type === 'mail') {
                 if(this.mail === ''){// 入力されていない場合
-                    mail_error_message.push(message[1]);
+                    mail_error_message.push(message[1]); //エラーメッセージを配列に格納
                 }else if (this.mail.indexOf('@') == -1 ) {// @が含まれていない場合(メールアドレスの形式でない場合)
-                    mail_error_message.push("メールアドレスの形式ではありません");
+                    mail_error_message.push("メールアドレスの形式ではありません");//エラーメッセージを配列に格納
                 }
-                this.errors.mail = mail_error_message
+                this.errors.mail = mail_error_message //エラーメッセージをerrors.mailに格納
             }
             // 件名
             else if (type === 'subject') {
                 if(this.subject === ''){// 入力されていない場合
-                    subject_error_message.push(message[1]);
+                    subject_error_message.push(message[1]); //エラーメッセージを配列に格納
                 }else if (this.subject.length > max) {// 入力された文字数がmaxを超えている場合
-                    subject_error_message.push(message[0]);
+                    subject_error_message.push(message[0]); //エラーメッセージを配列に格納
                 }
-                this.errors.subject = subject_error_message
+                this.errors.subject = subject_error_message //エラーメッセージをerrors.subjectに格納
             }
             // メッセージ
             else if (type === 'contents') {
                 if(this.contents === ''){// 入力されていない場合
-                    contents_error_message.push(message[1]);
+                    contents_error_message.push(message[1]); //エラーメッセージを配列に格納
                 }if (this.contents.length > max) {// 入力された文字数がmaxを超えている場合
-                    contents_error_message.push(message[0]);
+                    contents_error_message.push(message[0]); //エラーメッセージを配列に格納
                 }
-                this.errors.contents = contents_error_message
+                this.errors.contents = contents_error_message //エラーメッセージをerrors.contentsに格納
             }
         },
     },
